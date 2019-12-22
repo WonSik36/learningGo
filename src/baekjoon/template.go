24634d17b0a5f57287333e41a4b25e1309a5825e
wonsik <cws0173@gmail.com>
2019-12-22 19:06:10 +0900

    make template

---
+++ src/baekjoon/template.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"strconv"
+	"strings"
+	"sort"
+)
+
+func main(){
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+
+	w.Flush()
+}
+
+func getString(r *bufio.Reader) string{
+	str,_ := r.ReadString('\n')
+	str = strings.TrimSpace(str)
+
+	return str
+}
+
+func getStrTkn(r *bufio.Reader) []string{
+	str,_ := r.ReadString('\n')
+	str = strings.TrimSpace(str)
+	strTkn := strings.Split(str," ")
+
+	return strTkn
+}
+
+func printArr(arr []int, w *bufio.Writer) {
+	for i:=0;i<len(arr);i++ {
+		w.WriteString(strconv.Itoa(arr[i])+"\n")
+	}
+}
