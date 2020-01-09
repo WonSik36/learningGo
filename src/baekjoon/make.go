34f4c14e0b1c85da57b9bbb03f0b4aece4e4882c
wonsik <cws0173@gmail.com>
2020-01-09 16:42:36 +0900

    update template

--- src/baekjoon/make.go
+++ src/baekjoon/make.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"os"
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strconv"
 )
 
-func main(){
+func main() {
 	var pNum int
 
 	// get problem number
 	fmt.Print("Problem number: ")
-	_,err := fmt.Scan(&pNum)
+	_, err := fmt.Scan(&pNum)
 	checkError(err)
 
 	// get directory path of input problem number & template
-	dir,err := os.Getwd() 
+	dir, err := os.Getwd()
 	checkError(err)
-	temp := fmt.Sprintf(dir+"\\src\\baekjoon\\template.go") 
-	dir = fmt.Sprintf(dir+"\\src\\baekjoon\\BOJ"+strconv.Itoa(pNum)+".go") 
+	temp := fmt.Sprintf(dir + "\\src\\baekjoon\\template.go")
+	dir = fmt.Sprintf(dir + "\\src\\baekjoon\\BOJ" + strconv.Itoa(pNum) + ".go")
 	fmt.Println(dir)
 
 	// check if problem number is dupliacted
 	if _, err := os.Stat(dir); !os.IsNotExist(err) {
 		fmt.Println("File already exist")
 		return
-	} 
+	}
 
 	// get template file
-	dat,err := ioutil.ReadFile(temp)
+	dat, err := ioutil.ReadFile(temp)
 	checkError(err)
 
 	// get destination file
-	err = ioutil.WriteFile(dir,dat,os.FileMode(0644))
+	err = ioutil.WriteFile(dir, dat, os.FileMode(0644))
 	checkError(err)
 
 	fmt.Println("Make file succeed")
 }
 
-func checkError(err error){
+func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
--- src/baekjoon/template.go
+++ src/baekjoon/template.go
@@ -1,43 +1,49 @@
 /*
-    baekjoon online judge
-    problem number 
-    https://www.acmicpc.net/problem/
+   baekjoon online judge
+   problem number
+   https://www.acmicpc.net/problem/
 */
 
 package main
 
 import (
-	"os"
 	"bufio"
+	"os"
 	"strconv"
 	"strings"
-	"sort"
 )
 
-func main(){
+func main() {
 	r := bufio.NewReader(os.Stdin)
 	w := bufio.NewWriter(os.Stdout)
+	var sb strings.Builder
 
 	w.Flush()
 }
 
-func getString(r *bufio.Reader) string{
-	str,_ := r.ReadString('\n')
+func getInt(r *bufio.Reader) int {
+	res, _ := strconv.Atoi(getString(r))
+
+	return res
+}
+
+func getString(r *bufio.Reader) string {
+	str, _ := r.ReadString('\n')
 	str = strings.TrimSpace(str)
 
 	return str
 }
 
-func getStrTkn(r *bufio.Reader) []string{
-	str,_ := r.ReadString('\n')
+func getStrTkn(r *bufio.Reader) []string {
+	str, _ := r.ReadString('\n')
 	str = strings.TrimSpace(str)
-	strTkn := strings.Split(str," ")
+	strTkn := strings.Split(str, " ")
 
 	return strTkn
 }
 
 func printArr(arr []int, w *bufio.Writer) {
-	for i:=0;i<len(arr);i++ {
-		w.WriteString(strconv.Itoa(arr[i])+"\n")
+	for i := 0; i < len(arr); i++ {
+		w.WriteString(strconv.Itoa(arr[i]) + "\n")
 	}
 }
