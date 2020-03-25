91bc8e23a4125cde779d90022680b48c8827b9ba
wonsik <cws0173@gmail.com>
2020-03-25 11:09:46 +0900

    problem number 14681 https://www.acmicpc.net/problem/14681

---
+++ src/baekjoon/BOJ14681.go
@@ -0,0 +1,72 @@
+/*
+	baekjoon online judge
+	problem number 14681
+	https://www.acmicpc.net/problem/14681
+*/
+
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+
+	x := getInt(r)
+	y := getInt(r)
+
+	if x > 0 && y > 0 {
+		w.WriteString("1\n")
+	} else if x > 0 && y < 0 {
+		w.WriteString("4\n")
+	} else if x < 0 && y > 0 {
+		w.WriteString("2\n")
+	} else {
+		w.WriteString("3\n")
+	}
+
+	w.Flush()
+}
+
+func getInt(r *bufio.Reader) int {
+	res, _ := strconv.Atoi(getString(r))
+
+	return res
+}
+
+func getString(r *bufio.Reader) string {
+	str, _ := r.ReadString('\n')
+	str = strings.TrimSpace(str)
+
+	return str
+}
+
+func getStrTkn(r *bufio.Reader) []string {
+	str, _ := r.ReadString('\n')
+	str = strings.TrimSpace(str)
+	strTkn := strings.Split(str, " ")
+
+	return strTkn
+}
+
+func getIntArr(r *bufio.Reader) []int {
+	strTkn := getStrTkn(r)
+	arr := make([]int, len(strTkn), len(strTkn))
+
+	for i := 0; i < len(arr); i++ {
+		arr[i], _ = strconv.Atoi(strTkn[i])
+	}
+
+	return arr
+}
+
+func printArr(arr []int, w *bufio.Writer) {
+	for i := 0; i < len(arr); i++ {
+		w.WriteString(strconv.Itoa(arr[i]) + "\n")
+	}
+}
